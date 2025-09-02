0c3e67102227864305870b426fb308c1fcb0cebd
Bulumko-Ndema <bulumko.ndema@gmail.com>
2025-09-02 21:59:05 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // tells Next.js to use src/app
+  },
+};
+
+module.exports = nextConfig;
